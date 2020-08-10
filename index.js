6a0eb9ddaab05dbbc9e2b10556f1da2a64b120cd
ybaha <ybahaerarslan@gmail.com>
2020-08-10 23:17:55 +0300

    can draw photo now

--- index.js
+++ index.js
@@ -11,29 +11,37 @@ imageDiv.appendChild(canvas)
 const inputs = document.getElementsByClassName("input")
 const button = document.getElementById("btn")
 
+const Senacanvas = document.createElement("canvas");
 
 let optionsArray = []
 
 let widthC = optionsArray[0]
 let heightC = optionsArray[1]
-
-let changeRate = optionsArray[2]
-
-let pixelSize = optionsArray[3]
+let pixelSize = optionsArray[2]
+let changeRate = optionsArray[3]
 
 
 const inputCheck = () => {
-  let j = 0
+  let j = 0, senaC = 0
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].value !== "") {
       optionsArray[i] = parseInt(inputs[i].value)
       j++
     }
+
+    if (inputs[i].value === "1" && i !== 2) {
+      senaC++
+    }
+
   }
 
   if (j === inputs.length) {
     draw(optionsArray[0], optionsArray[1], optionsArray[2], optionsArray[3])
     j = 0
+  }
+
+  if (senaC == inputs.length - 1){
+    senaVerici()
   }
 }
 
@@ -58,6 +66,18 @@ const randomizer = (rgbValue, changeRate) => {
   return rgbValue
 }
 
+function getColor(x, y, canvas) {
+  var context = canvas.getContext("2d");
+  var pixel = context.getImageData(x, y, 1, 1);
+
+  var rgb = pixel.data;
+
+  let nRgb = Array.from(rgb);
+  nRgb.pop();
+
+  return nRgb;
+}
+
 const draw = (widthC, heightC, pixelSize, changeRate) => {
 
   let initialRgb = [Math.random() * 255, Math.random() * 255, Math.random() * 255]
@@ -66,10 +86,6 @@ const draw = (widthC, heightC, pixelSize, changeRate) => {
   canvas.height = heightC
 
   let row = []
-
-  let column = []
-
-  let lastOfRow = []
 
   for (let j = 0; j < heightC; j += pixelSize) {
     row = []
@@ -80,12 +96,6 @@ const draw = (widthC, heightC, pixelSize, changeRate) => {
       if (i == 0) {
         rgb = initialRgb
       }
-
-      // && j == 0
-
-      // else if (i == 0 && j != 0){
-      //   rgb = lastOfRow
-      // }
 
       else {
         rgb = row[(i / pixelSize) - 1]
@@ -100,16 +110,122 @@ const draw = (widthC, heightC, pixelSize, changeRate) => {
 
       row.push(rgb)
 
-      // if(i == widthC){
-      //   lastOfRow = rgb
-      // }
-
     }
-
-    // column.push(row)
 
   }
 
 }
 
-draw(400,400,5,1)
+
+draw(400, 400, 5, 1)
+
+
+const senaVerici = () => {
+
+  PSRate = optionsArray[2]
+
+  canvas.style.display = "none"
+
+  
+
+  Senacanvas.style.display = "block"
+  Senacanvas.style.margin = "0 auto"
+
+  imageDiv.appendChild(Senacanvas);
+
+  Senacanvas.width = 400;
+  Senacanvas.height = 400;
+
+  var ctxS = Senacanvas.getContext("2d");
+
+  // context.drawImage(image, 0, 0);
+
+
+  for (let j = 0; j < 400; j += PSRate) {
+
+    for (let i = 0; i < 400; i += PSRate) {
+
+      ctxS.fillStyle = `rgb(${senaArray[j][i][0]},${senaArray[j][i][1]},${senaArray[j][i][2]})`
+      ctxS.fillRect(i, j, PSRate, PSRate)
+
+    }
+
+  }
+}
+
+
+
+let sRow = []
+let sCol = []
+
+const pixelizator = () => {
+
+
+  for (let j = 0; j < 400; j++) {
+    sRow = []
+    for (let i = 0; i < 400; i++) {
+
+      sRow[i] = getColor(i,j,Senacanvas)
+
+
+    }
+
+    sCol.push(sRow)
+
+  }
+
+
+}
+
+const drawSena = (PSRate, array) => {
+
+  const nCanvas = document.createElement("canvas")
+
+  document.body.appendChild(nCanvas)
+
+  nCanvas.width = 400
+  nCanvas.height = 400
+
+
+
+  const ctxS = nCanvas.getContext("2d")
+
+  for (let j = 0; j < 400; j += PSRate) {
+
+    for (let i = 0; i < 400; i += PSRate) {
+
+      ctxS.fillStyle = `rgb(${array[j][i][0]},${array[j][i][1]},${array[j][i][2]})`
+      ctxS.fillRect(i, j, PSRate, PSRate)
+
+    }
+
+  }
+
+}
+
+// let saveData = (data, fileName) => { 
+//   var a = document.createElement("a")
+//   document.body.appendChild(a)
+//   a.style = "display: none"
+//   var json = JSON.stringify(data), 
+//       blob = new Blob([json], {type: "octet/stream"}), 
+//       url = window.URL.createObjectURL(blob)
+//   a.href = url
+//   a.download = fileName
+//   a.click()
+//   window.URL.revokeObjectURL(url)
+// }
+
+
+// var data = {senaArray}
+// fileName = "my-download.json"
+
+// let importSenaArray = () => {
+//   var obj = JSON.parse(contacts)
+//   return obj
+// }
+
+
+
+
+
